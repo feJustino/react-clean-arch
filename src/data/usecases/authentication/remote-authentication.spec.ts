626f1f0b9faddb771329a049fdc18c4186a2e5eb
Justino <fwxjustino@gmail.com>
2022-01-12 18:36:48 -0300

    test: adc parametros genericos http-post-client

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -5,14 +5,19 @@ import faker from 'faker'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { AccountModel } from '@/domain/models/account-models'
 
 type SutTypes = {
   sut: RemoteAuthentication
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy()
+  const httpPostClientSpy = new HttpPostClientSpy<
+    AuthenticationParams,
+    AccountModel
+  >()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
   return {
     sut,
@@ -43,6 +48,7 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(mockAuthentication())
     await expect(promise).rejects.toThrow(new InvalidCredentialsError())
   })
+
   test('Should throw UnexpectedError if HttpPostClient return 400', async () => {
     const { httpPostClientSpy, sut } = makeSut()
     httpPostClientSpy.response = {
@@ -51,6 +57,7 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(mockAuthentication())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
   test('Should throw UnexpectedError if HttpPostClient return 500', async () => {
     const { httpPostClientSpy, sut } = makeSut()
     httpPostClientSpy.response = {
@@ -59,6 +66,7 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(mockAuthentication())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
   test('Should throw UnexpectedError if HttpPostClient return 404', async () => {
     const { httpPostClientSpy, sut } = makeSut()
     httpPostClientSpy.response = {
